6a1909d0e7e831493f5a23c81a6b29c80f3fe508
unknown <audric.dc@gmail.com>
2020-10-13 17:28:42 +0200

    Initial commit

---
+++ velib-get-stations.py
@@ -0,0 +1,19 @@
+import json
+import time
+import urllib.request
+
+from kafka import KafkaProducer
+
+API_KEY = "0bffc32310873d0c5a8903db0582813f012b1269"  # FIXME Set your own API key here
+url = r"https://api.jcdecaux.com/vls/v1/stations?apiKey={}".format(API_KEY)
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10, 2))
+
+while True:
+    response = urllib.request.urlopen(url)
+    stations = json.loads(response.read().decode())
+    for station in stations:
+        producer.send("velib-stations-test", json.dumps(station).encode(), key=str(station["number"]).encode())
+        print("Sent : {}".format(json.dumps(station).encode()))
+    print("{} Produced {} station records".format(time.time(), len(stations)))
+    time.sleep(1)
