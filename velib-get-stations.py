eae65efbab07db6f1fa6100a6ef003f40407a57a
AudricDC <audric.dc@gmail.com>
2020-10-14 11:44:49 +0200

    Hide api key

--- velib-get-stations.py
+++ velib-get-stations.py
@@ -4,7 +4,7 @@ import urllib.request
 
 from kafka import KafkaProducer
 
-API_KEY = "0bffc32310873d0c5a8903db0582813f012b1269"  # FIXME Set your own API key here
+API_KEY = "myApiKey"  # FIXME Set your own API key here
 url = r"https://api.jcdecaux.com/vls/v1/stations?apiKey={}".format(API_KEY)
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10, 2))
